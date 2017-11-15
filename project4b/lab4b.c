6a21effbd9a05affe52e353d1e3cb81a72244eb3
Rishabh Jain <rishabh.jain1198@gmail.com>
2017-11-14 17:41:08 -0800

    Project 4B completed

--- project4b/lab4b.c
+++ project4b/lab4b.c
@@ -19,6 +19,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <math.h>
+#include<unistd.h>
 
 int ignoreReading = 0;
 long periodAmount = 1;
@@ -28,6 +29,7 @@ int loggingEnabled = 0;
 FILE *logFile;
 int loggingToggle;
 int true = 1;
+int generateReports = 1;
 
 mraa_aio_context sensVar;
 mraa_gpio_context butVar;
@@ -46,9 +48,11 @@ void myTime(char *unixTime){
 void turnOff(){
     char time[20];
     myTime(time);
-    
-    if(loggingEnabled){
-        fprintf(logFile, "%s\n", "OFF");
+
+    fprintf(stdout, "%s %s\n", time, "SHUTDOWN");
+
+    if(loggingEnabled)	{
+	fprintf(logFile, "%s\n", "OFF");
         fprintf(logFile, "%s %s\n", time, "SHUTDOWN");
     }
     
@@ -57,6 +61,7 @@ void turnOff(){
 }
 
 void printStartToggle(int toggle){
+    generateReports = toggle;
     if(loggingEnabled){
         if(toggle == 0)
             fprintf(logFile, "STOP\n");
@@ -67,6 +72,7 @@ void printStartToggle(int toggle){
 }
 
 void printScaleToggle(char toggle){
+    scale = toggle;
     if(loggingEnabled){
         if(toggle == 'C')
             fprintf(logFile, "SCALE=C\n");
@@ -85,15 +91,26 @@ void changePeriod(char *buf){
     return;
 }
 
-//FROM OFFICIAL SEEDDOC GROVE WIKI
 float properTemp(int a){
-    float R = 100000.0 * (1023.0/((float)a)-1.0);
-    int B = 4275;
-    return 1.0/(log(R/100000.0)/B+1/298.15)-273.15;
+
+    float R5V = 1023.0/a-1.0;
+    float R3V3 = 660.0/a-1.0;	//FROM PIAZZA
+    int R0 = 100000;
+    R5V = R0*R5V; R3V3 = R0*R3V3;
+    int B = 4275; 
+    float temperature = 0.0;
+
+    int USEFIVEVOLTAGE = 1;
+
+    if(USEFIVEVOLTAGE)
+	temperature = 1.0/(log(R5V/R0)/B+1/298.15)-273.15;	
+    else
+	temperature = 1.0/(log(R3V3/R0)/B+1/298.15)-273.15;
+
+    return temperature;
 }
 
 void * readButton(){
-    char buf[1024];
     while(true){
         int reading = mraa_gpio_read(butVar);
         if(reading){
@@ -109,7 +126,7 @@ void * readButton(){
 
 void * logTemperature(){
     while (true) {
-        
+	if(generateReports){
         float temp = properTemp(mraa_aio_read(sensVar));
         if(scale == 'F')
             temp = temp*1.8 + 32.0;
@@ -125,6 +142,7 @@ void * logTemperature(){
         if(!ignoreReading) ignoreReading=1; //only once
         
         sleep(periodAmount);
+}
     }
     return NULL;
 }
@@ -137,7 +155,7 @@ void * readInput(){
         
         if(!ignoreReading) continue;
         
-        int readAmount = read(STDIN_FILENO, buf, 512);
+        int readAmount = read(STDIN_FILENO, buf, 1024);
         
         if(readAmount < 0) exit(1);
         
@@ -145,7 +163,6 @@ void * readInput(){
             int i; int previousReceived = 0;
             for(i = 0; i < readAmount; i++){
                 if(buf[i] == '\n'){
-                    
                     buf[i] = 0 ;
                     if(strcmp(buf + previousReceived, "OFF") == 0) turnOff();
                     else if(strncmp(buf + previousReceived, "PERIOD=", strlen("PERIOD=")) == 0) changePeriod(buf + previousReceived);
@@ -195,16 +212,18 @@ int main(int argc, char **argv){
                 break;
             default:
                 fprintf(stderr, "Please enter valid argument!\n");
-                exit(2);
+                exit(1);
         }
     }
     
     if(loggingEnabled)
         logFile = fopen(logFileName, "a");
      
-    
-    sensVar = mraa_aio_init(0);
-    butVar = mraa_gpio_init(3);
+    sensVar = mraa_aio_init(1);
+    butVar = mraa_gpio_init(73);
+ if(!butVar) {fprintf(stderr, "BUTTON NOT FOUND\n"); exit(1);}
+
+if(!sensVar) {fprintf(stderr, "SENSOR NOT WORKING\n"); exit(1); }
 
     pthread_t *threads = malloc(3 * sizeof(pthread_t));
 
@@ -223,4 +242,6 @@ int main(int argc, char **argv){
   
     mraa_aio_close(sensVar);
     mraa_gpio_close(butVar);
+
+    return 0;
 }
